b4e829dbf3bbdbe2f5134bb687e7dda685e513a5
Emily M <30880308+e0marina@users.noreply.github.com>
2020-03-25 19:51:24 -0700

    updated model, orm and controller files

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -2,6 +2,11 @@
 
 //    * Express
 //    * `burger.js`
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
 
 // 4. Create the `router` for the app, and export the `router` at the end of your file.
 
--- models/burger.js
+++ models/burger.js
@@ -1,12 +1,24 @@
-// #### Model setup
-
-//     * Inside `burger.js`, import `orm.js` into `burger.js`
-
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 //     * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
-var burger = {};
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
 //     * Export at the end of the `burger.js` file.
 // Export the database functions for the controller
 module.exports = burger;
