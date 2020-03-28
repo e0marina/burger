e1f08c325f362bb25fbbca5841f028c7f675230d
Emily M <30880308+e0marina@users.noreply.github.com>
2020-03-27 19:06:24 -0700

    updated server.js file and port listening

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -9,6 +9,7 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // 4. Create the `router` for the app, and export the `router` at the end of your file.
+//from the demo it seems like everything happens on the index page...
 
 // Export routes for server.js to use.
 module.exports = router;
