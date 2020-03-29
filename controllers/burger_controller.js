0eac8b7208d4d60a172897fa337ddf5600bda0eb
Emily M <30880308+e0marina@users.noreply.github.com>
2020-03-29 15:34:44 -0700

    able to get data to go into Mysql

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,6 +5,44 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // 4. Create the `router` for the app, and export the `router` at the end of your file.
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var burgerObj = {
+      burgers: data
+    };
+    console.log(burgerObj);
+    res.render("index", burgerObj);
+  });
+});
 
+router.post("/", function(req, res) {
+  burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    function(result) {
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
+router.put("/", function(req, res) {
+  var condition = "id" + req.params.id;
+  console.log("condition", condition);
+
+  burger.updateOne(
+    {
+      devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
 // Export routes for server.js to use.
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -2,7 +2,24 @@
 var orm = require("../config/orm.js");
 
 //     * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
-var burger = {};
+var burger = {
+  //Select all of the burger names and ids
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
 //     * Export at the end of the `burger.js` file.
 // Export the database functions for the controller
 module.exports = burger;
