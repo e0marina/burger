f0eb84b3e01bbdc156ac431c8b320e87de816a1a
Emily M <30880308+e0marina@users.noreply.github.com>
2020-03-29 14:27:38 -0700

    trying something new

--- models/burger.js
+++ models/burger.js
@@ -2,23 +2,7 @@
 var orm = require("../config/orm.js");
 
 //     * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
-};
+var burger = {};
 //     * Export at the end of the `burger.js` file.
 // Export the database functions for the controller
 module.exports = burger;
