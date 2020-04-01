9c1c21c64787758cf94e3858acaf3f8b80afd869
Emily M <30880308+e0marina@users.noreply.github.com>
2020-03-31 21:56:35 -0700

    updated jawsdb info

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,34 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Wolverines06!",
-  database: "burgers_db"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  var sql = "DELETE FROM burgers";
-  connection.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log("Number of records deleted: " + result.affectedRows);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Wolverines06!",
+    database: "burgers_db"
   });
+}
 
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// // Make connection.
+// connection.connect(function(err) {
+//   var sql = "DELETE FROM burgers";
+//   connection.query(sql, function(err, result) {
+//     if (err) throw err;
+//     console.log("Number of records deleted: " + result.affectedRows);
+//   });
 
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
